4761e9f1e38ffd456d9e8929ce96f55816d149ee
lakatosdorottya <lakatos.dorottya.05@gmail.com>
2024-08-04 20:49:35 +0200

    Fejlesztés

---
+++ Class1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace I4Pfeladatok
+{
+    internal class TitkositasOsztaly
+    {
+        public string uzenet;
+        public string kulcs;
+        public string kodolt;
+
+
+        public TitkositasOsztaly(string p_uzenet, string p_kulcs, string p_kodolt)
+        {
+            uzenet = p_uzenet;
+            kulcs = p_kulcs;
+            kodolt = p_kodolt;
+        }
+        public TitkositasOsztaly() : this("", "", "")
+        {
+
+        }
+
+        public string Rejtjelezes()
+        {
+            kodolt = ""; //rejtjelezett üzenet
+            int szam = 0; //rejtjelezett üzenet i. karakterének kódja
+            int n = 0;
+            if (uzenet.Length > kulcs.Length)
+            {
+                n = kulcs.Length;
+            }
+            else
+            {
+                n = uzenet.Length;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                char nu = uzenet[i]; //i. vizsgált karaktere az üzenetnek
+                char nk = kulcs[i]; //i. vizsgált karaktere a kulcsnak
+                szam = Program.abc[nu] + Program.abc[nk];
+                szam = szam % 27;
+                kodolt += Program.revabc[szam];
+
+            }
+            return kodolt;
+        }
+        public string Dekódolás()
+        {
+            uzenet = ""; //dekódolt üzenet
+            int n = 0;
+            if (kodolt.Length > kulcs.Length)
+            {
+                n = kulcs.Length;
+            }
+            else
+            {
+                n = kodolt.Length;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int szamrejt = 0; //kódolt üzenet i. karakterének kódja
+                int szamkulcs = 0; //kulcs i. karakterének kódja
+                int szamdekod = 0; //dekódolt üzenet i. karakterének kódja
+                char nrejt = kodolt[i]; //rejtett uzenet i. karaktere
+                char nkulcs = kulcs[i]; //kulcs i. karaktere
+                szamrejt = Program.abc[nrejt];
+                szamkulcs = Program.abc[nkulcs];
+                if (szamrejt < szamkulcs)
+                {
+                    szamdekod = (szamrejt + 27) - szamkulcs;
+                    uzenet += Program.revabc[szamdekod];
+                }
+                else
+                {
+                    szamdekod = szamrejt - szamkulcs;
+                    uzenet += Program.revabc[szamdekod];
+                }
+            }
+            return uzenet;
+        }
+        public string Visszafejtes()
+        {
+            kulcs = ""; //dekódolt üzenet
+            int n = 0;
+            if (kodolt.Length < uzenet.Length)
+            {
+                n = kodolt.Length;
+            }
+            else
+            {
+                n = uzenet.Length;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int szamrejt = 0; //kódolt üzenet i. karakterének kódja
+                int szamkulcs = 0; //kulcs i. karakterének kódja
+                int szamdekod = 0; //dekódolt üzenet i. karakterének kódja
+                char nrejt = kodolt[i]; //rejtett uzenet i. karaktere
+                char nuzenet = uzenet[i]; //uzenet i. karaktere
+                szamrejt = Program.abc[nrejt];
+                szamkulcs = Program.abc[nuzenet];
+                if (szamrejt < szamkulcs)
+                {
+                    szamdekod = (szamrejt + 27) - szamkulcs;
+                    kulcs += Program.revabc[szamdekod];
+                }
+                else
+                {
+                    szamdekod = szamrejt - szamkulcs;
+                    kulcs += Program.revabc[szamdekod];
+                }
+            }
+            return kulcs;
+        }
+        public JoeErtek Joe()
+        {
+            uzenet = Dekódolás();
+            List<string> words = new List<string>();
+            words.AddRange(uzenet.Split(" "));
+            for (int i = 0; i < words.Count - 1; i++)
+            {
+                if (!Program.szavak.Contains(words[i]))
+                {
+                    return JoeErtek.NemJo;
+                }
+
+            }
+            for (int i = 0; i < Program.szavak.Count; i++)
+            {
+                if (Program.szavak[i].StartsWith(words[words.Count - 1]))
+                {
+                    return JoeErtek.ReszbenJo;
+                }
+            }
+            return JoeErtek.NemJo;
+        }
+
+    }
+
+
+    public enum JoeErtek
+    {
+        NemJo,
+        ReszbenJo,
+        TeljesenJo
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 using System.Collections.Generic;
 
-namespace óbudai
+namespace I4Pfeladatok
 {
-    internal class Program
+    public class Program
     {
-        static List<string> szavak;
-        static Dictionary<char, int> abc;
-        static Dictionary<int, char> revabc;
-        static List<string> JoKulcsok;
+        public static List<string> szavak;
+        public static Dictionary<char, int> abc;
+        public static Dictionary<int, char> revabc;
+        public static List<string> JoKulcsok;
 
         static void Main(string[] args)
         {
@@ -28,146 +28,84 @@ namespace óbudai
 
             //1. feladat
             //üzenet és kód rejtjelezése
+            TitkositasOsztaly elso = new TitkositasOsztaly();
             Console.WriteLine("1. feladat: Üzenet és kód rejtett üzenetté alakítása");
             Console.Write("Írja ide az üzenetet! ");
-            string uzenet = Console.ReadLine();
+            elso.uzenet = Console.ReadLine();
             Console.Write("Írja ide a kódot! ");
-            string kulcs1 = Console.ReadLine();
-            if (uzenet.Length > kulcs1.Length)
+            elso.kulcs = Console.ReadLine();
+            if (elso.uzenet.Length > elso.kulcs.Length)
             {
                 Console.WriteLine("A kulcs nem elég hosszú.");
             }
             else
             {
 
-                Console.WriteLine("A rejtjelezett üzenet: {0}", Rejtjelezes(uzenet, kulcs1));
+                Console.WriteLine("A rejtjelezett üzenet: {0}", elso.Rejtjelezes());
             }
 
             //rejtjelezett üzenet dekódolása
+            TitkositasOsztaly masodik = new TitkositasOsztaly();
             Console.WriteLine("");
             Console.WriteLine("Kódolt üzenet dekódolása kód segítségével");
             Console.Write("Írja ide a kódolt üzenetet! ");
-            string kodolt = Console.ReadLine(); //kódolt üzenet
+            masodik.kodolt = Console.ReadLine(); //kódolt üzenet
             Console.Write("Írja ide a kulcsot! ");
-            string kulcs2 = Console.ReadLine();
-            Console.WriteLine("A dekódolt üzenet: {0}", Dekódolás(kodolt, kulcs2));
-            
+            masodik.kulcs = Console.ReadLine(); masodik.Dekódolás();
+
             //2. feladat
-             Console.WriteLine("2. feladat");
+            TitkositasOsztaly negyedik = new TitkositasOsztaly();
+            Console.WriteLine("2. feladat");
             Console.Write("Írja ide az első kódolt üzenetet! ");
-            string uz1 = Console.ReadLine();
-            Console.Write("Írja ide a második kódolt üzenetet! "); 
-            string uz2 = Console.ReadLine();
+            string kodolt1 = "ebtobehpzmjnmfqwuirlazvslpl"; // Console.ReadLine();
+            Console.Write("Írja ide a második kódolt üzenetet! ");
+            negyedik.kodolt = "cvtlsxo fiutxysspjzxtxwp"; // Console.ReadLine();
 
-            Kozoskulcs(uz1, uz2, "");
+            Console.WriteLine("");
+            Console.WriteLine("");
 
+            Kozoskulcs(kodolt1, negyedik, "");
+
+            Console.WriteLine("");
+            Console.WriteLine("");
             Console.WriteLine("A talált közös kulcsok:");
             foreach (string kulcs in JoKulcsok)
             {
-                if ((kulcs.Length >= uz1.Length) || (kulcs.Length >= uz2.Length))
+                if ((kulcs.Length == kodolt1.Length) || (kulcs.Length == negyedik.kodolt.Length))
                 {
-                    Console.WriteLine(kulcs);
+                    Console.WriteLine("-" + kulcs + "-");
                 }
             }
         }
-
-        static string Rejtjelezes(string uzenet, string kulcs1)
+        static void Kozoskulcs(string p_kodolt, TitkositasOsztaly p_negyedik, string p_kulcs)
         {
-            string ruzenet = ""; //rejtjelezett üzenet
-            int szam = 0; //rejtjelezett üzenet i. karakterének kódja
-            int n = 0;
-            if (uzenet.Length > kulcs1.Length)
-            {
-                n = kulcs1.Length;
-            }
-            else
-            {
-                n = uzenet.Length;
-            }
-            for (int i = 0; i < n; i++)
-            {
-                char nu = uzenet[i]; //i. vizsgált karaktere az üzenetnek
-                char nk = kulcs1[i]; //i. vizsgált karaktere a kulcsnak
-                szam = abc[nu] + abc[nk];
-                szam = szam % 27;
-                ruzenet += revabc[szam];
-
-            }
-            return ruzenet;
-        }
-        static string Dekódolás(string kodolt, string kulcs2)
-        {
-            string uzenet = ""; //dekódolt üzenet
-            int n = 0;
-            if (kodolt.Length > kulcs2.Length)
-            {
-                n = kulcs2.Length;
-            }
-            else
-            {
-                n = kodolt.Length;
-            }
-            for (int i = 0; i < n; i++)
-            {
-                int szamrejt = 0; //kódolt üzenet i. karakterének kódja
-                int szamkulcs = 0; //kulcs i. karakterének kódja
-                int szamdekod = 0; //dekódolt üzenet i. karakterének kódja
-                char nrejt = kodolt[i]; //rejtett uzenet i. karaktere
-                char nkulcs = kulcs2[i]; //kulcs i. karaktere
-                szamrejt = abc[nrejt];
-                szamkulcs = abc[nkulcs];
-                if (szamrejt < szamkulcs)
-                {
-                    szamdekod = (szamrejt + 27) - szamkulcs;
-                    uzenet += revabc[szamdekod];
-                }
-                else
-                {
-                    szamdekod = szamrejt - szamkulcs;
-                    uzenet += revabc[szamdekod];
-                }
-            }
-            return uzenet;
-        }
-
-        static bool Joe(string uz2, string kulcs)
-        {
-            string dekuz2 = Dekódolás(uz2, kulcs); //2. üzenet dekódolva
-            List<string> words = new List<string>();
-            words.AddRange(dekuz2.Split(" "));
-            for (int i = 0; i < words.Count - 1; i++)
-            {
-                if (!szavak.Contains(words[i]))
-                {
-                    return false;
-                }
-
-            }
-            for (int i = 0; i < szavak.Count; i++)
-            {
-                if (szavak[i].StartsWith(words[words.Count - 1]))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-        static void Kozoskulcs(string uz1, string uz2, string kulcs)
-        {
-            if ((uz1.Length <= kulcs.Length) || (uz2.Length <= kulcs.Length))
+            Console.WriteLine(" - '" + p_kulcs + "'");
+            if ((p_kodolt.Length <= p_kulcs.Length) || (p_negyedik.kodolt.Length <= p_kulcs.Length))
             {
                 return;
             }
-            string dekuz1 = Dekódolás(uz1, kulcs); //1. üzenet dekódolva
+            TitkositasOsztaly harmadik = new TitkositasOsztaly();
+            harmadik.kulcs = p_kulcs;
+            harmadik.kodolt = p_kodolt;
+            harmadik.uzenet = harmadik.Dekódolás(); //1. üzenet dekódolva
+            string VizsgaltUzenet = harmadik.uzenet;
             for (int i = 0; i < szavak.Count; i++)
             {
-                string tesztuz1 = dekuz1 + " " + szavak[i];
-                tesztuz1 = tesztuz1.Trim();
-                kulcs = Dekódolás(uz1, tesztuz1);
-                if (Joe(uz2, kulcs))
+                harmadik.uzenet = VizsgaltUzenet + " " + szavak[i];
+                harmadik.uzenet = harmadik.uzenet.Trim();
+                Console.WriteLine("    = '" + harmadik.uzenet + "'");
+                p_kulcs = harmadik.Visszafejtes();
+                p_negyedik.kulcs = p_kulcs;
+                JoeErtek eredmeny = p_negyedik.Joe();
+                if (eredmeny != JoeErtek.NemJo)
                 {
-                    JoKulcsok.Add(kulcs);
-                    Kozoskulcs(uz1, uz2, kulcs);
+
+                    if (eredmeny == JoeErtek.TeljesenJo)
+                    {
+                        JoKulcsok.Add(p_kulcs);
+                    }
+                    
+                    Kozoskulcs(p_kodolt, p_negyedik, p_kulcs);
                 }
             }
             return;
